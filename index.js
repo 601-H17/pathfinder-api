bf0772e7597e030028dc5df298fb922b1d2ed935
MacTHEgenius <levesque.benoit@me.com>
2017-02-02 11:09:47 -0500

    Create project API with NodeJS for pathfinding (GET & POST)

---
+++ index.js
@@ -0,0 +1,78 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var PathFinder = require('geojson-path-finder');
+
+var geojson = require('./corridors.json');
+
+// Configure body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Set port
+var port = process.env.PORT || 8080;
+
+
+/* ! Routes ! */
+var router = express.Router();
+
+// GET /
+router.get('/', function(req, res){
+  var localA = req.query.localA, localB = req.query.localB;
+  var path;
+  var status;
+  var error = undefined;
+  console.log(localA);
+  console.log(localB);
+
+  var fs = require('fs');
+  try {
+    var start = findLocal(localA);
+    var finish = findLocal(localB);
+
+    function findLocal(toFind) {
+        var file = fs.readFileSync('./corridors.json');
+        var obj = JSON.parse(file);
+        for(var i=0;i<obj.features.length;i++){
+            if(obj.features[i].geometry.type == "Point" && obj.features[i].properties.ref != null && obj.features[i].properties.ref == toFind){
+                return obj.features[i];
+            }
+        }
+    }
+
+    var pathFinder = new PathFinder(geojson);
+    path = pathFinder.findPath(start, finish);
+  } catch (e) {
+    error = { message:"Can't find path with those locals" };
+  }
+
+  if (error == undefined) {
+    res.status(200).json(path);
+  } else {
+    res.status(404).json({ error });
+  }
+});
+
+router.post('/corridors', function(req, res) {
+  var corridors = req.body.map;
+  var string = JSON.stringify(corridors, null, '\t');
+  console.log(corridors);
+  var fs = require('fs');
+  fs.writeFile("./corridors.json", string, function(err) {
+      if(err) {
+          return console.log(err);
+          res.status(400).json({message:"File not saved", error: err});
+      }
+      console.log("The file was saved!");
+      res.status(201).json({message:"File saved successfully"});
+  });
+});
+
+app.use('/api/pathfinder', router);
+
+
+/* ! Start ! */
+
+app.listen(port);
+console.log('Server start on port ' + port);
+console.log('CTRL + C to close');
