2580666b540c8ab6afc51f39181b89f109b75ffd
Fred Laberge <fred.laberge7@gmail.com>
2017-02-06 11:34:25 -0500

    First api test with mocha and chai

--- test/pathfinder.js
+++ test/pathfinder.js
@@ -1,16 +1,21 @@
-// process.env.NODE_ENV = 'test';
+process.env.NODE_ENV = 'test';
 
-// let chai = require('chai');
-// let chaiHttp = require('chai-http');
-// let server = require('../index');
-// let should = chai.should();
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
 
-// chai.use(chaiHttp);
+chai.use(chaiHttp);
 
-var assert = require('assert');
-describe('/GET book', () => {
-    it('it should GET all the books', (done) => {
-        assert.equal(-1, [1, 2, 3].indexOf(4));
+describe('/GET pathfinder', () => {
+    it('it should find the path', (done) => {
+        chai.request(server.app)
+            .get('/api/pathfinder');
+        // .end((err, res) => {
+        //     res.should.have.status(200);
+        //     res.body.should.be.a('array');
+        //     res.body.length.should.be.eql(0);
         done();
+        // });
     });
 });
