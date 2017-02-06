dff5a03fce5293f11edb47c7afa20ea2c3a184ce
Fred Laberge <fred.laberge7@gmail.com>
2017-02-06 10:43:53 -0500

    Add chai to projet for testing

---
+++ test/pathfinder.js
@@ -0,0 +1,21 @@
+process.env.NODE_ENV = 'test';
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../index');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('/GET book', () => {
+    it('it should GET all the books', (done) => {
+        chai.request(server)
+            .get('/book')
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                res.body.length.should.be.eql(0);
+                done();
+            });
+    });
+});
