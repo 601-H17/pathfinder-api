3ae3a06d096964d542ea64ef98fb65155129dd36
Joel Lambert <jlambert443@gmail.com>
2017-02-19 16:06:20 -0500

    Add function to find local and staircases on same floor

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ router.get('/', function(req, res) {
 
         var pathFinder = new PathFinder(geojson);
         path = pathFinder.findPath(start, finish);*/
-        algoTools.pathfind();
+        algoTools.pathfind("G-160", "G-164");
     } catch (e) {
         error = { message: "Can't find path with those locals" };
     }
