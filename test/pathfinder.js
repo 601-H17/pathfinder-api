958e32117d124bcdcda482ad3eb94c8249325ab4
Frédéric Bérubé <fredob@hotmail.ca>
2017-02-28 13:32:20 -0500

    Adding tests and changing others

--- test/pathfinder.js
+++ test/pathfinder.js
@@ -29,33 +29,77 @@ var startLocal = 'G-165';
 var endLocal = 'G-164';
 var localPoint;
 
-var result = { path: 
-                [ [ -71.2874189411, 46.78601674403 ],
-                  [ -71.28741905245, 46.78600546398 ],
-                  [ -71.28729974824, 46.78601907973 ],
-                  [ -71.28730301717, 46.78602796227 ] ],
-              weight: 0.011485373846850676 };
 
-describe('Pathfinder', function() {
+
+describe('Pathfinder of locals on same floor and wing', function() {
+    beforeEach(function(){
+        const FIRST_FLOOR_LOCAL = 'G-165';
+        const SAME_FLOOR_LOCAL = 'G-164';
+        const EXPECTED_PATHFIND_RESULT = { path: 
+                                                [ [ -71.2874189411, 46.78601674403 ],
+                                                [ -71.28741905245, 46.78600546398 ],
+                                                [ -71.28729974824, 46.78601907973 ],
+                                                [ -71.28730301717, 46.78602796227 ] ],
+                                            weight: 0.011485373846850676 };
+    });
+
     describe('#pathfind(startLocal, endLocal)', function() {
-        it('should return a localization of the local when the local to find is given', async () => {
-        var path = await algorithmPathfinder.pathfind(startLocal, endLocal);
-        expect(path.path).to.eql(result.path);
-        expect(path.weight).to.eql(result.weight);
+        context('when the local to find is given', function(){
+            it('should return the path to the local', async () => {
+
+                //Arrange
+                const PATH_PROPERTY = 'path';
+                const WEIGHT_PROPERTY = 'weight';
+
+                //Act
+                var fullpath = await algorithmPathfinder.pathfind(FIRST_FLOOR_LOCAL, SAME_FLOOR_LOCAL);
+
+                //Assert
+                expect(fullpath).to.have.property(PATH_PROPERTY , result.path);
+                expect(fullpath).to.have,property(WEIGHT_PROPERTY , result.weight);
+            });
         });
     });
     
-    describe('#pathfind(startLocal, endLocal) with higher floor and same wing', function() {
-        it('should return a localization of the local when the local was find', async () => {
-            const RESULT_LENGTH = 2;
+    describe('#pathfind(startLocal, endLocal)', function() {
+        context('when the local was found', function(){
+            it('should return the total weight distance of the path', async () => {
 
-            var fullpath = await algorithmPathfinder.pathfind(startLocal, endLocal);
-            
-            expect(fullpath.path).to.eql(resultTestHigherFloor.path);
-            expect(fullpath.weight).to.eql(resultTestHigherFloor.weight);
-            expect(fullpath).to.have.lengthOf(RESULT_LENGTH)
+                //Arrange
+                const RESULT_LENGTH = 2;
+                const TOTAL_WEIGHT_PROPERTY = 'totalWeight';
+
+                //Act
+                var fullpath = await algorithmPathfinder.pathfind(FIRST_FLOOR_LOCAL, SAME_FLOOR_LOCAL);
+
+                //Assert
+                expect(fullpath).to.have.lengthOf(RESULT_LENGTH);
+                expect(fullpath).to.have.property(TOTAL_WEIGHT_PROPERTY, EXPECTED_PATHFIND_RESULT.weight);
+            });
         });
     });
     
 });
+/*
+describe('Pathfinder of locals on different floor but same wing', function(){
+    before(function(){
+        const FIRST_FLOOR_LOCAL = 'G-165';
+        const SECOND_FLOOR_LOCAL = 'G-273';
+        //RESULT
+    });
 
+    describe('#pathfind(startLocal, endLocal)', function(){
+        context('when the local was found', function(){
+            it('should return a path to the local through staircases', async () => {
+                //Arrange
+                const RESULT_LENGTH = 3;
+                //Act
+                var fullpath = await algorithmPathfinder.pathfind(FIRST_FLOOR_LOCAL, SECOND_FLOOR_LOCAL);
+                //Assert
+                expect(fullpath).to.have.lengthOf(RESULT_LENGTH);
+                expect(fullpath).to.have
+            });
+        });
+    });
+});
+*/
